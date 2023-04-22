e4043f3d89f9a0d69100c95b03ac16b6086bb488
mindong2 <qlsnaos12@naver.com>
2023-04-22 22:03:07 +0900

    recap recoil get, set

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
-import TodoList from "./components/TodoList";
 import Global from "./style/Global";
+import { hourSelector, minuteState } from "./atom/atoms";
+import { useRecoilState } from "recoil";
 
 const App = () => {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+
+  const onChangeMinute = (e: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(Number(e.currentTarget.value));
+  };
+
+  const onChangeHour = (e: React.FormEvent<HTMLInputElement>) => {
+    setHours(Number(e.currentTarget.value));
+  };
+
   return (
     <>
       <Global />
-      <TodoList />
+      <input value={minutes} onChange={onChangeMinute} type="number" />
+      <input value={hours} onChange={onChangeHour} type="number" />
     </>
   );
 };
