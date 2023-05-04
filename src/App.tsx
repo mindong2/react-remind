d3b65319283b0ccc9b9f155aae3672a7cae807d6
Dongmin Kim <qlsnaos12@naver.com>
2023-05-04 11:23:42 +0900

    스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,31 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import TvShow from "./components/TvShow";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import styled from "styled-components";
+
+const AppContainer = styled.main`
+    font-family: "Noto Sans KR", sans-serif;
+`;
 
 const App = () => {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/tv" element={<TvShow />} />
-        {/* <Route path='/search' element={<Search />} /> */}
-      </Routes>
-    </>
-  );
+    return (
+        <AppContainer>
+            <HelmetProvider>
+                <Helmet>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500&display=swap" rel="stylesheet" />
+                </Helmet>
+            </HelmetProvider>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/tv" element={<TvShow />} />
+                {/* <Route path='/search' element={<Search />} /> */}
+            </Routes>
+        </AppContainer>
+    );
 };
 
 export default App;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,46 +8,46 @@ const HomeWrap = styled.div``;
 const Loader = styled.div``;
 
 const Banner = styled.div<{ bgPhoto: string }>`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url(${({ bgPhoto }) => bgPhoto});
-  background-size: cover;
-  height: 100vh;
-  padding-left: 60px;
-  color: #fff;
-  p {
-    line-height: 1.5;
-  }
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.8)), url(${({ bgPhoto }) => bgPhoto});
+    background-size: cover;
+    height: 100vh;
+    padding-left: 60px;
+    color: #fff;
+    p {
+        line-height: 1.5;
+    }
 `;
 
 const Title = styled.h2`
-  font-size: 58px;
+    font-size: 58px;
 `;
 
 const OverView = styled.p`
-  width: 50%;
-  margin-top: 30px;
-  font-size: 30px;
+    width: 50%;
+    margin-top: 30px;
+    font-size: 30px;
 `;
 
 const Home = () => {
-  const { isLoading, data } = useQuery<IMovie>(["movie", "nowPlaying"], getMovies);
+    const { isLoading, data } = useQuery<IMovie>(["movie", "nowPlaying"], getMovies);
 
-  return (
-    <HomeWrap>
-      {isLoading ? (
-        <Loader>Loading/...</Loader>
-      ) : (
-        <>
-          <Banner bgPhoto={movieImageName(data?.results[0].backdrop_path || "")}>
-            <Title>{data?.results[0].title}</Title>
-            <OverView>{data?.results[0].overview}</OverView>
-          </Banner>
-        </>
-      )}
-    </HomeWrap>
-  );
+    return (
+        <HomeWrap>
+            {isLoading ? (
+                <Loader>Loading/...</Loader>
+            ) : (
+                <>
+                    <Banner bgPhoto={movieImageName(data?.results[0].backdrop_path || "")}>
+                        <Title>{data?.results[0].title}</Title>
+                        <OverView>{data?.results[0].overview}</OverView>
+                    </Banner>
+                </>
+            )}
+        </HomeWrap>
+    );
 };
 
 export default Home;
