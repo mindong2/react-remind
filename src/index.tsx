e47bef3042f706551532c26caa595055ebe5727d
Dongmin Kim <qlsnaos12@naver.com>
2023-05-11 16:54:38 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 // react18부터 일반 리액트쿼리 사용안된다..
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import {RecoilRoot} from 'recoil'
+import { RecoilRoot } from "recoil";
 import App from "./App";
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </RecoilRoot>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <RecoilRoot>
+                <QueryClientProvider client={queryClient}>
+                    <App />
+                </QueryClientProvider>
+            </RecoilRoot>
+        </BrowserRouter>
+    </React.StrictMode>
 );
